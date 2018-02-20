9a6cf2d864377b926fb45d8a651b71c670c3f4b3
Serhii <sergey.petrunya@gmail.com>
2018-02-20 13:41:57 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  artistName: 'songTitle'
+}
+
+var updatePlaylist = (playlist, artistName, songTitle) => {
+  // playlist = playlist;
+  // playlist.artistName = artistName;
+  // playlist.songTitle = songTitle;
+  return playlist
+}
