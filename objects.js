2b4fcfa5833f3c4a22d0c69b528aabebfd48e03a
Serhii <sergey.petrunya@gmail.com>
2018-02-20 14:16:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,5 +7,5 @@ var updatePlaylist = (playlist, artistName, songTitle) => {
 }
 
 var removeFromPlaylist = (playlist, artistName) => {
-  delete playlist.artistName;
+  delete playlist[artistName];
 }
